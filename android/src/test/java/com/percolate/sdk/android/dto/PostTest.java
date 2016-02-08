7250bbfa3b5c9d2c9819751c8633b2cec0d33e4c
Brent Watson <brent@brentwatson.com>
2016-02-08 13:34:26 -0500

    JaCoCo codecoverage setup for android module

---
+++ android/src/test/java/com/percolate/sdk/android/dto/PostTest.java
@@ -0,0 +1,13 @@
+package com.percolate.sdk.android.dto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostTest {
+
+    @Test
+    public void test() {
+        assertTrue(new Post().testIt());
+    }
+}
