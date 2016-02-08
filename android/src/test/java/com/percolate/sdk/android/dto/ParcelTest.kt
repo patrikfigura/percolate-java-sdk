4d1330e0a6fc186c3114b2433e10a745a23d53e6
Brent Watson <brent@brentwatson.com>
2016-02-08 13:44:23 -0500

    Converted ParcelTest class to kotlin.

---
+++ android/src/test/java/com/percolate/sdk/android/dto/ParcelTest.kt
@@ -0,0 +1,14 @@
+package com.percolate.sdk.android.dto
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ParcelTest {
+
+    @Test
+    fun testParceling() {
+        //TODO -- Test object parceling.
+        Post();
+    }
+}
