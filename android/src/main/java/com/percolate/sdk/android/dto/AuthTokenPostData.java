0f8ce4183ebeb66d3e497c6c521dba92e9280167
Brent Watson <brent@brentwatson.com>
2016-03-29 10:07:21 -0400

    Unit test fix

---
+++ android/src/main/java/com/percolate/sdk/android/dto/AuthTokenPostData.java
@@ -0,0 +1,44 @@
+package com.percolate.sdk.android.dto;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.HashMap;
+
+/**
+ * Android version of {@link com.percolate.sdk.dto.AuthTokenPostData}.  Implements {@link Parcelable}
+ */
+public class AuthTokenPostData extends com.percolate.sdk.dto.AuthTokenPostData implements Parcelable {
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.code);
+        dest.writeString(this.grantType);
+        dest.writeMap(this.extraFields);
+    }
+
+    public AuthTokenPostData() {
+    }
+
+    protected AuthTokenPostData(Parcel in) {
+        this.code = in.readString();
+        this.grantType = in.readString();
+        this.extraFields = new HashMap<>();
+        in.readMap(this.extraFields, HashMap.class.getClassLoader());
+    }
+
+    public static final Creator<AuthTokenPostData> CREATOR = new Creator<AuthTokenPostData>() {
+        public AuthTokenPostData createFromParcel(Parcel source) {
+            return new AuthTokenPostData(source);
+        }
+
+        public AuthTokenPostData[] newArray(int size) {
+            return new AuthTokenPostData[size];
+        }
+    };
+}
--- core/src/main/java/com/percolate/sdk/dto/AuthTokenPostData.java
+++ core/src/main/java/com/percolate/sdk/dto/AuthTokenPostData.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.percolate.sdk.interfaces.HasExtraFields;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.io.Serializable;
 import java.util.HashMap;
@@ -25,6 +27,11 @@ public class AuthTokenPostData implements Serializable, HasExtraFields {
     @JsonIgnore
     protected Map<String, Object> extraFields = new HashMap<>();
 
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+    
     public String getCode() {
         return code;
     }
