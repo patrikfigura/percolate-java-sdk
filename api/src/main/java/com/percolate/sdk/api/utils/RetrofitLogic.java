fa9141aa25a4a502e73ff94bd266227aa8b101ba
Brent Watson <brent@brentwatson.com>
2016-03-31 14:14:05 -0400

    Make RetrofitLogic constructor public

--- api/src/main/java/com/percolate/sdk/api/utils/RetrofitLogic.java
+++ api/src/main/java/com/percolate/sdk/api/utils/RetrofitLogic.java
@@ -39,7 +39,7 @@ public class RetrofitLogic {
     /**
      * Construct instance.
      */
-    RetrofitLogic(@NotNull PercolateApi context) {
+    public RetrofitLogic(@NotNull PercolateApi context) {
         this.context = context;
     }
 
