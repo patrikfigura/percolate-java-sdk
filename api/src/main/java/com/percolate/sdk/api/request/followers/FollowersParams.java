6abad01a22f48cb9a5b50b9ffb9fd396bf2b3cdc
Mohit Sarveiya <mohitsya@gmail.com>
2016-05-03 13:36:49 -0400

    added limit to followers request

--- api/src/main/java/com/percolate/sdk/api/request/followers/FollowersParams.java
+++ api/src/main/java/com/percolate/sdk/api/request/followers/FollowersParams.java
@@ -11,6 +11,7 @@ public class FollowersParams {
     private Map<String, Object> params = new HashMap<>();
 
     public FollowersParams() {
+      params.put("limit", "1000");
     }
 
     public FollowersParams objectUid(String objectUid) {
