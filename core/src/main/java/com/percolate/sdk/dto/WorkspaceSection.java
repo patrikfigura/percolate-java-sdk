65298bdc8ce3bf3beafec090fb807d8f578f1029
Mohit Sarveiya <mohitsya@gmail.com>
2016-05-20 17:48:08 -0400

    Revise sorting logic

--- core/src/main/java/com/percolate/sdk/dto/WorkspaceSection.java
+++ core/src/main/java/com/percolate/sdk/dto/WorkspaceSection.java
@@ -62,9 +62,9 @@ public class WorkspaceSection implements Serializable, HasExtraFields, Comparabl
     @Override
     public int compareTo(@NotNull WorkspaceSection other) {
         if (other.ordinal < ordinal) {
-          return -1;
-        } else if (ordinal > other.ordinal) {
           return 1;
+        } else if (ordinal > other.ordinal) {
+          return -1;
         } else {
           return 0;
         }
