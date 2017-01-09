3bbce2a398a020f1df2837183cab13386fcd5eba
Brent Watson <brent@brentwatson.com>
2017-01-09 10:05:45 -0500

    Add scope_ids param to features endpoint

--- api/src/main/java/com/percolate/sdk/api/request/features/FeaturesParams.java
+++ api/src/main/java/com/percolate/sdk/api/request/features/FeaturesParams.java
@@ -1,6 +1,9 @@
 package com.percolate.sdk.api.request.features;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +18,11 @@ public class FeaturesParams {
 
     public FeaturesParams scopeId(String scopeId) {
         params.put("scope_id", scopeId);
+        return this;
+    }
+
+    public FeaturesParams scopeIds(List<String> scopeIds) {
+        params.put("scope_ids", StringUtils.join(scopeIds, ","));
         return this;
     }
 
