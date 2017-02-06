889a4b324f8257b7ae7db46c945b368f7fbcf2cb
Mohit Sarveiya <mohitsya@gmail.com>
2017-02-06 16:52:16 -0500

    Add to string to preference names type

--- core/src/main/java/com/percolate/sdk/enums/PreferenceNameType.java
+++ core/src/main/java/com/percolate/sdk/enums/PreferenceNameType.java
@@ -1,9 +1,17 @@
 package com.percolate.sdk.enums;
 
+import java.util.Locale;
+
 /**
  * Valid preference name types.
  */
 public enum PreferenceNameType {
     PLATFORMS,
-    PREFERRED_TAGS
+    PREFERRED_TAGS;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase(Locale.getDefault());
+    }
+
 }
