dbd7516cdba09d58ee39d0f4df87da0a9f967a71
Patrik Figura <patrik.figura@hotovo.org>
2018-02-05 17:21:14 +0100

    DROID-653: Determine if the v3/auth endpoint is being used removed method

--- api/src/main/java/com/percolate/sdk/api/request/authorization/UserRolesService.java
+++ api/src/main/java/com/percolate/sdk/api/request/authorization/UserRolesService.java
@@ -15,9 +15,6 @@ import java.util.Map;
  */
 interface UserRolesService {
 
-    @GET(Endpoints.API_V3_PATH + "/authorization/{user_id}")
-    Call<UserRoles> get(@Path("user_id") String userId, @QueryMap Map<String, Object> params);
-
     @GET(Endpoints.API_V5_PATH + "/user_role/")
     Call<UserRolesV5> get(@QueryMap Map<String, Object> params);
 }
